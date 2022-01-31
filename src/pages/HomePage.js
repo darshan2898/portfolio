3c7757c079f768c226b0b84d1015ddf83b86b522
Kakkarot28 <senkuchan2898@gmail.com>
2022-01-31 23:06:59 +0530

    color fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,16 @@ import HomeIndex from "../components/homeP/HomeIndex";
 const HomePage = () => {
   return (
     <>
+      <p className="alert">
+        <a
+          href="https://github.com/darshan2898?tab=repositories"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          click for other projects and source code. My github profile
+          (darshan2898/github) & contact:-9867581671.
+        </a>
+      </p>
       <HomeIndex />
     </>
   );
